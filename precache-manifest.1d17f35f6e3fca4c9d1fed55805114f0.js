2d6cc637b68559c8ee3568d6d01878abdfb3cfa2
hhori64 <hhori64@gmail.com>
2021-03-22 06:52:15 +0900

    Updates

---
+++ precache-manifest.1d17f35f6e3fca4c9d1fed55805114f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9e2ab5662f300d33e52e67b4188aa5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cada876e459bde6e0bc7",
+    "url": "/monsters-rolodex/static/css/main.8445548c.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cada876e459bde6e0bc7",
+    "url": "/monsters-rolodex/static/js/main.2184d5b9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
